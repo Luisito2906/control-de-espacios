aa9df2cfa196b2e3ec36f3f80a1e2e07ed8e6e3b
luisito <luisitoxd@MacBook-Air-de-luisito.local>
2017-10-17 11:49:18 -0500

    Initial Commit

---
+++ control de espacios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  control de espacios
+//
+//  Created by luisito on 17/10/17.
+//  Copyright © 2017 luisito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
